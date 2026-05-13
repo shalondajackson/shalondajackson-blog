7f8de8d5dfddd64bdc12e3b8e6482593a31fd85b
Shalonda Jackson <106330004+shalondajackson@users.noreply.github.com>
2026-05-12 21:11:01 -0400

    Add blog post and image

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Shalonda Jackson | Healthcare Operations & Systems';
+export const SITE_DESCRIPTION = 'Exploring healthcare operations, systems thinking, productivity, career growth, and intentional living while building sustainable success one step at a time';
